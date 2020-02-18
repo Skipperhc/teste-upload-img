b2f3a8d96bc850075a36daf91294e3d618a2f32f
skipperhc <vitorhainosz2@gmail.com>
2020-02-17 23:21:03 -0300

    teste para upload de img
    não sei o que eu usei, mas tá la, vejo amanha

---
+++ teste-upload-img/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+using teste_upload_img.Models;
+
+namespace teste_upload_img.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHostingEnvironment _env;
+
+        public HomeController(IHostingEnvironment environment)
+        {
+            _env = environment;
+        }
+        
+        [HttpPost] public IActionResult Uploadimg(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                var imagePath = @"upload\imagens\";
+                var uploadPath = _env.WebRootPath + imagePath;
+                
+                //create repository
+                if (!Directory.Exists(uploadPath))
+                {
+                    Directory.CreateDirectory(uploadPath);
+                }
+                
+                //create uniq file name
+                var uniqFileName = Guid.NewGuid().ToString();
+                var filename = Path.GetFileName(uniqFileName + "." + file.FileName.Split(".")[1].ToLower());
+                string fullPath = uploadPath + filename;
+
+                imagePath = imagePath + @"\";
+                var filePath = @".." + Path.Combine(imagePath, filename);
+
+                using (var fileStream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                }
+
+
+                ViewData["FileLocation"] = filePath;
+            }
+            return View(nameof(Index));
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
---
+++ teste-upload-img/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace teste_upload_img.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
